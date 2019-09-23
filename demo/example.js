fc79d2da527efa92ca618d19143c09f44aa2eca1
Mateusz Rohde <mateusz.rohde@gmail.com>
2019-09-23 12:28:44 +0200

    ng-form support

--- demo/example.js
+++ demo/example.js
@@ -5,99 +5,105 @@
  */
 
 angular.module('exampleApp', ['validate'])
-	.controller('exampleCtrl', ['$rootScope', function($rootScope){
-		this.ex1 = {
-			first_name: 'Mark',
-			last_name: 'Otto',
-			username: '',
-			gender: '',
-			check: false,
-			radio: '',
-			submitted: false
-		};
-		this.ex2 = {
-			first_name: 'Mark',
-			last_name: 'Otto',
-			username: '',
-			gender: '',
-			check: false,
-			radio: '',
-			submitted: false
-		};
-		this.ex3 = '';
-		this.ex4 = {
-			required: '',
-			min: -1,
-			max: 11,
-			pattern: 'aaa',
-			number: '',
-			email: '123',
-			ngRequired: '',
-			ngPattern: 'aaa',
-			ngMinlength: '1',
-			ngMaxlength: '123'
-		};
-		this.ex5 = {
-			url: 'not an url',
-			host: 'not a host',
-			urls: "http://example.com\nhttps://www.example.com",
-			hosts: "example.com\nwww.example.com",
-			eq1: '1',
-			eq2: '2',
-      custom: 'test'
-		};
+    .controller('exampleCtrl', ['$rootScope', function($rootScope) {
+        this.ex1 = {
+            first_name: 'Mark',
+            last_name: 'Otto',
+            username: '',
+            gender: '',
+            check: false,
+            radio: '',
+            submitted: false
+        };
+        this.ex2 = {
+            first_name: 'Mark',
+            last_name: 'Otto',
+            username: '',
+            gender: '',
+            check: false,
+            radio: '',
+            submitted: false
+        };
+        this.ex3 = '';
+        this.ex4 = {
+            required: '',
+            min: -1,
+            max: 11,
+            pattern: 'aaa',
+            number: '',
+            email: '123',
+            ngRequired: '',
+            ngPattern: 'aaa',
+            ngMinlength: '1',
+            ngMaxlength: '123'
+        };
+        this.ex5 = {
+            url: 'not an url',
+            host: 'not a host',
+            urls: 'http://example.com\nhttps://www.example.com',
+            hosts: 'example.com\nwww.example.com',
+            eq1: '1',
+            eq2: '2',
+            custom: 'test'
+        };
+        this.ngfEx = {
+            first_name: 'Mark',
+            last_name: '',
+            username: '',
+            gender: ''
+        };
 
-		this.validateCustom = function(value){
-		  return value === 'test';
-    };
+        this.validateCustom = function(value) {
+            return value === 'test';
+        };
 
-		$rootScope.nav = [];
-	}])
-	/**
-	 * @ngdoc component
-	 * @name codeExample
-	 */
-	.component('codeExample', {
-		transclude: true,
-		bindings: {
-			html: '<'
-		},
-		controllerAs: 'ctrl',
-		template: '<div class="card-footer"><pre><code>{{ctrl.html}}</code></pre></div>'
-	})
-	/**
-	 * @ngdoc directive
-	 * @name codeExampleHook
-	 */
-	.directive('codeExampleHook', ['$compile', function($compile){
-		return {
-			restrict: 'A',
-			priority: 999999999999,
-			compile: function(element){
-				let html = element.clone().removeAttr('code-example-hook')[0].innerHTML,
-					whitespace = html.match(/^\s+/)[0].replace(/\n/, '');
-				html = html.replace(new RegExp('^' + whitespace, 'gm'), '').trim();
-				return function(scope){
-					const newScope = scope.$new(),
-						newElement = angular.element('<code-example html="html"></code-example>');
-					newScope.html = html;
-					element.after(newElement);
-					$compile(newElement)(newScope);
-				}
-			}
-		};
-	}])
-	.directive('pageHeader', [function(){
-		return {
-			restrict: 'C',
-			controller: ['$rootScope', '$element', '$attrs', function($rootScope, $element, $attrs){
-				if(angular.isUndefined($attrs.id)){
-					return;
-				}
-				$rootScope.nav.push({
-					id: $attrs.id,
-					title: $element.text().trim()
-				});
-			}]
-		}
-	}]);
+        $rootScope.nav = [];
+    }])
+    /**
+     * @ngdoc component
+     * @name codeExample
+     */
+    .component('codeExample', {
+        transclude: true,
+        bindings: {
+            html: '<'
+        },
+        controllerAs: 'ctrl',
+        template: '<div class="card-footer"><pre><code>{{ctrl.html}}</code></pre></div>'
+    })
+    /**
+     * @ngdoc directive
+     * @name codeExampleHook
+     */
+    .directive('codeExampleHook', ['$compile', function($compile) {
+        return {
+            restrict: 'A',
+            priority: 999999999999,
+            compile: function(element) {
+                let html = element.clone().removeAttr('code-example-hook')[0].innerHTML,
+                    whitespace = html.match(/^\s+/)[0].replace(/\n/, '');
+                html = html.replace(new RegExp('^' + whitespace, 'gm'), '').trim();
+                return function(scope) {
+                    const newScope = scope.$new(),
+                        newElement = angular.element('<code-example html="html"></code-example>');
+                    newScope.html = html;
+                    element.after(newElement);
+                    $compile(newElement)(newScope);
+                }
+            }
+        };
+    }])
+    .directive('pageHeader', [function() {
+        return {
+            restrict: 'C',
+            controller: ['$rootScope', '$element', '$attrs', function($rootScope, $element, $attrs) {
+                if (angular.isUndefined($attrs.id)) {
+                    return;
+                }
+                $rootScope.nav.push({
+                    id: $attrs.id,
+                    title: $element.text().trim()
+                });
+            }]
+        }
+    }]);
--- dist/angularjs-bootstrap4-validate.js
+++ dist/angularjs-bootstrap4-validate.js
@@ -11,47 +11,76 @@ angular.module('validate', []);
  *  Copyright (c) 2019 Rodziu <mateusz.rohde@gmail.com>
  *  License: MIT
  */
-!function(){
-	'use strict';
+!function() {
+    'use strict';
 
-	/**
-	 *  @ngInject
-	 */
-	formDirective.$inject = ["validate"];
-	function formDirective(validate){
-		return {
-			restrict: 'E',
-			require: 'form',
-			link(scope, element, attrs, formCtrl){
-				if('novalidate' in attrs){
-					return;
-				}
+    /**
+     *  @ngInject
+     */
+    formDirective.$inject = ["validate"];
+    function formDirective(validate) {
+        return {
+            restrict: 'E',
+            require: 'form',
+            compile() {
+                return {
+                    pre(scope, element, attrs, formCtrl) {
+                        if ('novalidate' in attrs) {
+                            return;
+                        }
 
-				formCtrl.validationMode = attrs['validateMode'] || validate.mode;
+                        formCtrl.validationMode = attrs['validateMode'] || validate.mode;
 
-				element[0].addEventListener('submit', function(){
-					element.addClass('was-validated');
-				});
+                        element[0].addEventListener('submit', function() {
+                            element.addClass('was-validated');
+                        });
 
-				formCtrl.resetValidation = function(){
-					formCtrl.$setPristine();
-					angular.element(element[0].querySelectorAll('.was-validated')).removeClass('was-validated');
-					element.removeClass('was-validated');
-				}
+                        formCtrl.resetValidation = function() {
+                            formCtrl.$setPristine();
+                            angular.element(element[0].querySelectorAll('.was-validated')).removeClass('was-validated');
+                            element.removeClass('was-validated');
+                        };
 
-				formCtrl.showValidation = function(){
-          element.addClass('was-validated');
-        }
-			}
-		}
-	}
+                        formCtrl.showValidation = function() {
+                            element.addClass('was-validated');
+                        };
+                    }
+                }
+            }
+        };
+    }
 
-	/**
-	 * @ngdoc directive
-	 * @name form
-	 * @param {String} validateMode
-	 */
-	angular.module('validate').directive('form', formDirective);
+    function ngFormDirective() {
+        return {
+            restrict: 'EAC',
+            priority: -1,
+            require: ['form', '^^form'],
+            compile() {
+                return {
+                    pre(scope, element, attrs, ctrl) {
+                        const [formCtrl, parentFormCtrl] = ctrl;
+                        if (angular.isDefined(parentFormCtrl.validationMode)) {
+                            formCtrl.validationMode = parentFormCtrl.validationMode;
+                            ['resetValidation', 'showValidation'].forEach((fn) => {
+                                formCtrl[fn] = function() {
+                                    parentFormCtrl[fn]();
+                                };
+                            });
+                        }
+                    },
+                };
+            },
+        };
+    }
+
+    /**
+     * @ngdoc directive
+     * @name form
+     * @param {String} validateMode
+     */
+    angular.module('validate').directive('form', formDirective);
+
+    angular.module('validate').directive('ngForm', ngFormDirective);
 }();
 
 /*
